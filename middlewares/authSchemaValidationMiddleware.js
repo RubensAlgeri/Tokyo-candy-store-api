b096e988df8d1c358e2a0f66f7cab7cf6afdd8f8
Rubens Algeri <rubens_zys@hotmail.com>
2022-05-11 01:26:57 -0300

    adicionar login e cadastro

--- middlewares/authSchemaValidationMiddleware.js
+++ middlewares/authSchemaValidationMiddleware.js
@@ -1,7 +1,7 @@
 import authSchema from "../schemas/authSchema.js";
 
 export function validaUsuario(req, res, next) {
-    const validation = userSchema.validate(req.body);
+    const validation = authSchema.validate(req.body);
     if (validation.error) {
         return res.status(422).send(validation.error.details.map(details => details.message));
     }
