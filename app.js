e9412c64b5eef6a48b35d1f8799f574a894f0b9d
Rubens Algeri <rubens_zys@hotmail.com>
2022-05-10 23:55:58 -0300

    adicionar login e cadastro

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors())
 app.use(express.json());
 
 app.use(router);
-const porta = process.env.PORT
+const porta = process.env.PORT || 5000;
 app.listen(porta, () => {
     console.log(`Server is listening on port ${porta}.`);
 });
